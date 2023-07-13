02f9130f1d2c47093c67b94d843de424366e1940
kstekels <kstekels+github@gmail.com>
2023-07-13 19:20:54 +0300

    Initial Commit

---
+++ AlamofireNetworkingDemo/AlamofireNetworkingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireNetworkingDemoApp.swift
+//  AlamofireNetworkingDemo
+//
+//  Created by Kārlis Štekels on 13/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireNetworkingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
